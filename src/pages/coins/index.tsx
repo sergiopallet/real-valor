4e3044257ea78cfb923b8def52ea79df8f42196c
Sergio Pallet <sergiopallet@gmail.com>
2022-02-25 19:16:05 -0300

    Adding dependency datepicker

--- src/pages/coins/index.tsx
+++ src/pages/coins/index.tsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
 import { Input } from "../../components";
 
+
+
 const Coins = () => {
 
-  const [value, setValue] = useState(3);
-  const [date, setDate] = useState(0);
+  const [value, setValue] = useState<number | string>();
+  const [date, setDate] = useState<string>("");
+
+  const setMOneyValue = (value: any) => {
+    setValue(value);
+  }
+
+  const search = () => {
+    console.log("pesquisando valores");
+  }
 
   return (
     <>
       <h1>coins</h1>
-      <Input />
+      value:
+      <Input value={value} onChange={({ target }) => { setMOneyValue(target.value) }} />
+      <br />
+      date:
+      <Input value={date} onChange={({ target }) => { setDate(target.value) }} />
+      <button onClick={() => search()}>Pesquisar</button>
     </>
   );
 }
